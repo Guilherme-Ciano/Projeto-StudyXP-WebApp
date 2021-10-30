7521b0b9de1dd361617fce2a81dcbf4c69f07f30
Guilherme Ciano <gui_ciano31@yahoo.com>
2021-10-30 16:19:05 -0300

    changes: landing-page

--- src/app/views/Professores/config-prof/config-prof.component.ts
+++ src/app/views/Professores/config-prof/config-prof.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LogoutService } from 'src/app/services/logout.service';
+import { CriptografiaService } from './../../../services/criptografia.service';
 
 @Component({
   selector: 'app-config-prof',
@@ -9,8 +10,20 @@ import { LogoutService } from 'src/app/services/logout.service';
 export class ConfigProfComponent implements OnInit {
 
   constructor(
-    private sair: LogoutService
+    private sair: LogoutService, private criptoService : CriptografiaService,
   ) { }
+
+  rawData = this.criptoService.descriptografar(localStorage.getItem('Raw_Data'), 'md5')
+  jsonData = JSON.parse(this.rawData)
+  
+  user = {
+    Nome: this.jsonData.nome,
+    Email: this.jsonData.email,
+    Pass: this.jsonData.password,
+    Phone: this.jsonData.phone,
+    ID: this.jsonData.id,
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/welcome-screen/welcome-screen.component.ts
+++ src/app/welcome-screen/welcome-screen.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome-screen',
   templateUrl: './welcome-screen.component.html',
-  styleUrls: ['./welcome-screen.component.css']
+  styleUrls: ['./welcome-screen.component.scss'],
 })
 export class WelcomeScreenComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
